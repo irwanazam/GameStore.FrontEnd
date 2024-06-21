d06bd074b359efcfbf36e6e48aa8966732611549
irwanazam <irwan.azam@enovade.com>
2024-06-21 22:40:31 +0800

    Update latest code

--- obj/Debug/net8.0/GameStore.FrontEnd.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStore.FrontEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+940c98979f380f32ec169781ea88fbd404ac53b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
